9740f2eb5e63e34f15e98b86d9b0f1f10f8a7749
prayagm <52482653+prayagm@users.noreply.github.com>
2020-09-11 20:04:38 +0530

    Customers get API added in appconstants

--- src/app/AppConstants.ts
+++ src/app/AppConstants.ts
@@ -2,3 +2,6 @@
 
 export const BaseServiceURL = "http://127.0.0.1:5000";
 export const CustomersRoute = "/customers";
+
+//Customers APIs
+export const GetAllCustomers = "/api/Customers/GetAllCustomers";
