e465603800dfa440ff1be1889396fded40331800
vishnusb1996 <52482641+vishnusb1996@users.noreply.github.com>
2020-09-11 12:45:13 +0530

    Added the route and data to app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './ProductOrdering/Home/Home.component';
 import { CustomerComponent } from './ProductOrdering/Customer/Customer.component';
+import { ProductComponent } from './ProductOrdering/Product/Product.component';
+import { CategoryComponent } from './ProductOrdering/Category/Category.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'customers', component: CustomerComponent },
+  { path: 'products', component: ProductComponent },
+  { path: 'category', component: CategoryComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './ProductOrdering/Home/Home.component';
 import { CustomerComponent } from './ProductOrdering/Customer/Customer.component';
 
+import { ProductComponent } from './ProductOrdering/Product/Product.component';
+import { CategoryComponent } from './ProductOrdering/Category/Category.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CustomerComponent
+    CustomerComponent,
+    ProductComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
